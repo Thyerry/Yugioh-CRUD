14761b3374229a069183d1fe1eee1e91cc9725bf
Thyerry Nunes <thyerry.nunes@gmail.com>
2019-06-18 00:57:37 -0300

    Tipo e Atributos de monstros

---
+++ YugiohDatabase/Enum/MonsterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YugiohDatabase.Enum {
+    public enum MonsterAttribute {
+        Light = 1,
+        Dark = 2,
+        Water = 3,
+        Earth = 4,
+        Fire = 5,
+        Wind = 6,
+        Divine = 7
+    }
+}
---
+++ YugiohDatabase/Enum/MonsterType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YugiohDatabase.Enum {
+    public enum MonsterType {
+        Aqua ,
+        Beast ,
+        Beast_Warrior ,
+        Creator_God ,
+        Cyberse ,
+        Dinossaur ,
+        Divine_Beast ,
+        Dragon ,
+        Fairy ,
+        Fiend ,
+        Fish ,
+        Insect ,
+        Machine ,
+        Plant ,
+        Psychic , 
+        Pyro ,
+        Reptile ,
+        Rock ,   
+        Sea_Serpent ,
+        Thunder ,
+        Spellcaster ,
+        Warrior ,
+        Winged_Beast ,
+        Wyrm ,
+        Zombie ,
+    }
+}
